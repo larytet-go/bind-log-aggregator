7765186c23eaa25405ccab7ded3f8f46d52b4233
arkady <larytet@yahoo.com>
2019-04-22 11:05:59 +0300

    Follow log file

--- bind-log-aggregator.go
+++ bind-log-aggregator.go
@@ -8,10 +8,20 @@ import (
 	"transactionlogger"
 )
 
-func main() {
+func getOldestLogfile(logFolder string) {
 
+}
+
+// Pick the oldest not empty file, read the content, parse, output,
+// remove the file content (keep the file itself), repeat
+func tailLog(logFolder string) {
+
+}
+
+func main() {
 	useUdp := flag.Bool("udp", false, "Use UDP for rsyslog")
-	loggerUrl := flag.String("logger", "", "rsyslog://127.0.0.1")
+	loggerUrl := flag.String("logger", "rsyslog://127.0.0.1", "For example 'stdout'")
+	logFolder := flag.String("logfolder", "/var/log/named", "The bind's log folder")
 	flag.Parse()
 
 	fmt.Printf("Aggregator is staring ...\n")
@@ -25,4 +35,5 @@ func main() {
 		}
 		time.Sleep(1 * time.Second)
 	}
+	go tailLog(*logFolder)
 }
